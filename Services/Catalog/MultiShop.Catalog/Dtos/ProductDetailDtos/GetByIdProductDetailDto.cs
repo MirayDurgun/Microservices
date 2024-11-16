1942cd86f9e3627ca98bc2f7e42581ab271d383d
Miray Durgun <miraydurgun67@gmail.com>
2024-11-16 23:09:04 +0300

    11. Ders sonu

---
+++ Services/Catalog/MultiShop.Catalog/Dtos/ProductDetailDtos/GetByIdProductDetailDto.cs
@@ -0,0 +1,9 @@
+﻿namespace MultiShop.Catalog.Dtos.ProductDetailDtos
+{
+	public class GetByIdProductDetailDto
+	{
+		public string ProductDetailIDd { get; set; }
+		public string ProductDetailDescription { get; set; }
+		public string ProductDetailInfo { get; set; }
+	}
+}
